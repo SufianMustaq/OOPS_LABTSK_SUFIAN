97fe05e3afeae63bdb595fd98dd2eff6898bdfa7
SufianMustaq <72722597+SufianMustaq@users.noreply.github.com>
2020-10-19 22:29:15 +0600

    Lab 1 completed

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        /*{/Answer 01
+            Console.WriteLine("Hello");
+            Console.WriteLine("Sufian Mustaq");
+        }*/
+
+        /*//Answer 02
+        {
+            int a, b, sum;
+            a = 50;
+            b = 50;
+            sum = a + b;
+            Console.WriteLine("The Sum of two number A and B is: " + sum);
+            Console.ReadLine();
+        }/*
+//Answer 3
+        {
+ int a, b, div;
+ a = 50;
+ b = 50;
+ div = b / a;
+ Console.WriteLine("The Dividation of two number A and B is: " + div);
+ Console.ReadLine();
+        }
+
+        //Answer 04
+        /*{
+            int a = (-5 + 8 * 6);
+            int b = ((55 + 9) % 9);
+            int c = (20 + -3 * 5 / 8);
+            int d = (5 + 15 / 3 * 2 - 8 % 3);
+            Console.WriteLine("The answer of A: " + a);
+            Console.WriteLine("The answer of b: " + b);
+            Console.WriteLine("The answer of c: " + c);
+            Console.WriteLine("The answer of d: " + d);
+            Console.ReadLine();
+        }*/
+        //Answer 05
+        /*{
+            int a, b, add, mul, sub, div;
+            a = 20;
+            b = 100;
+            add = a + b;
+            mul = a * b;
+            sub = b - a;
+            div = b / a;
+            Console.WriteLine("The Sum of two number A and B is: " + add);
+            Console.WriteLine("The Multipication of two number A and B is: " + mul);
+            Console.WriteLine("The substraction of two number A and B is: " + sub);
+            Console.WriteLine("The Dividation of two number A and B is: " + div);
+            Console.ReadLine();
+        }*/
+        //Answer 06
+        /*{
+            double e = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+            Console.WriteLine("compute the specified expressions is: " + e);
+            Console.WriteLine();
+        }*/
+
+        //Answer 07
+       /* {
+            double Width = 5.5, Height = 8.5;
+            double Perimeter = (2 * (Width + Height));
+            double Area = Width * Height;
+
+            Console.WriteLine("Area is 5.6 * 8.5 =" + Area);
+            Console.WriteLine("Perimeter is 2 * (5.6 + 8.5) =" + Perimeter);
+            Console.ReadLine();
+        }
+
+}
+}
